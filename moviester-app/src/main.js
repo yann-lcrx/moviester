a66a52fbb987c0a06faa05e98284727dcbcc11f5
yann-lcrx <yannick84v@gmail.com>
2021-10-10 00:43:38 +0200

    Removed unused dependencies

--- moviester-app/src/main.js
+++ moviester-app/src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(router).mount("#app");
