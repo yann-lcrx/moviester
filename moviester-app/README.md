3c72bc7570825f272e19bb64cff25b55cf188200
yann-lcrx <yannick84v@gmail.com>
2021-10-10 21:44:08 +0200

    API hotfix

--- moviester-app/README.md
+++ moviester-app/README.md
@@ -5,6 +5,12 @@
 npm install
 ```
 
+Upon installation, create a .env file in project root ('moviester-app') with your IMDB API key:
+
+```VUE_APP_TMDB_API_KEY = yourapikey```
+
+Obtain yours here: https://developers.themoviedb.org/3/getting-started/introduction
+
 ### Compiles and hot-reloads for development
 ```
 npm run serve
--- moviester-app/src/services/api/movies.js
+++ moviester-app/src/services/api/movies.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
+const apiKey = process.env.VUE_APP_TMDB_API_KEY;
+
 export default {
     getMovie(id) {
-        return axios.get(`movie/${id}?api_key=16607c0b583f536ffc3282bc6adfc080`)
+        return axios.get(`movie/${id}?api_key=${apiKey}`)
             .then(response => {
                 return response.data
             })
     },
     getActor(id) {
-        return axios.get(`person/${id}?api_key=16607c0b583f536ffc3282bc6adfc080`)
+        return axios.get(`person/${id}?api_key=${apiKey}`)
             .then(response => {
                 return response.data
             })
     },
     getMovieCredits(id) {
-        return axios.get(`movie/${id}/credits?api_key=16607c0b583f536ffc3282bc6adfc080`)
+        return axios.get(`movie/${id}/credits?api_key=${apiKey}`)
             .then(response => {
                 return response.data
             })
